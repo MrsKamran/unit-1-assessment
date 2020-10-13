5e4c7856991290960f63bf9e302209090fda42cb
Madiha Kamran <madihakamran@Madihas-MacBook-Pro.local>
2020-10-13 16:07:22 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var showResult = document.getElementsByTagName("h1")[0];
+var addButton = document.getElementById("add-button");
+var subtractButton = document.getElementById("subtract-button");
+var numberInput = document.getElementById("number-input");
+
+var result = 0;
+
+addButton.addEventListener('click', doAddition);
+subtractButton.addEventListener('click', doSubtraction);
+
+
+function doAddition(){
+    result = result + parseInt(numberInput.value) ;
+    displayResult()
+}
+
+function doSubtraction(){
+    result = result - parseInt(numberInput.value) ;
+    displayResult()
+}
+
+function displayResult(){
+    showResult.innerText = result;
+}
+
